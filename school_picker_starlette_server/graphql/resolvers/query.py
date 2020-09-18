fa3e74be82f467cb41becdabd9ecdb8c2383a2c2
Aphosis <aphosis.github@gmail.com>
2020-09-18 18:14:07 +0200

    ✨ added user queries

--- school_picker_starlette_server/graphql/resolvers/query.py
+++ school_picker_starlette_server/graphql/resolvers/query.py
@@ -1,9 +1,24 @@
 from ariadne import QueryType
 
+from ...db import database
+from ...sql import users
+from ...validation import User
+
 
 query = QueryType()
 
 
-@query.field("hello")
-async def resolve_hello(*_, name=None):
-    return f"Hello {name or 'guest'}"
+@query.field("users")
+async def resolve_users(*_):
+    query = users.select()
+    rows = await database.fetch_all(query)
+    results = [User(**row) for row in rows]
+    return results
+
+
+@query.field("user")
+async def resolve_user(*_, id):
+    query = users.select(users.c.id == id)
+    row = await database.fetch_one(query)
+    result = User(**row)
+    return result
