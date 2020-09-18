7efd934f5678c9f8f48aac13dbbe7c4e0a8c4d50
Aphosis <aphosis.github@gmail.com>
2020-09-18 18:16:15 +0200

    ✨ connect database

--- school_picker_starlette_server/app.py
+++ school_picker_starlette_server/app.py
@@ -1,22 +1,32 @@
-from starlette.applications import Starlette
-from starlette.routing import Mount
-from starlette.config import Config
 from ariadne.asgi import GraphQL
+from ariadne.graphql import GraphQLSchema
+from databases import Database
+from starlette.applications import Starlette
+from starlette.config import Config
+from starlette.routing import Mount
 
+from .config import config
+from .db import database
 from .graphql import schema
 
-config = Config(".env")
 
-
-def create_app(config: Config) -> Starlette:
+def create_app(
+    config: Config,
+    database: Database,
+    schema: GraphQLSchema,
+) -> Starlette:
     debug = config("DEBUG", cast=bool, default=False)
+
     app = Starlette(
         debug=debug,
         routes=[
             Mount("/graphql", GraphQL(schema=schema, debug=debug)),
         ],
+        on_startup=[database.connect],
+        on_shutdown=[database.disconnect],
     )
+
     return app
 
 
-app = create_app(config)
+app = create_app(config, database, schema)
